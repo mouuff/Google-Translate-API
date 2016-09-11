a7c8c87e56ad29b9695efb88621e000a0f1d607f
hobbyhack <shanegary@gmail.com>
2016-09-11 19:30:12 +0300

    Update GoogleTranslate_p3.py

--- python/GoogleTranslate_p3.py
+++ python/GoogleTranslate_p3.py
@@ -5,6 +5,14 @@ import urllib.request
 
 agent = {'User-Agent':"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)"}
 
+def convertUnicodeToHTMLEsc(text):
+    htmlEsc = str(text.encode()).replace("b\'\\x", "%").replace("\\x", "%").replace("\'", '')
+    return htmlEsc
+
+def translateFromUnicode(to_translate, to_language="auto", language="auto"):
+    htmlEsc = convertUnicodeToHTMLEsc(to_translate)
+    translation = translate(htmlEsc, to_language, language)
+    return (translation)
 
 def translate(to_translate, to_langage="auto", langage="auto"):
     """
